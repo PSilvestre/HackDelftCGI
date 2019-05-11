4e0c205412316047723067cb18b0892463671497
pedro <pmfsilvestre@gmail.com>
2019-05-11 22:15:05 +0200

    F commit messages

---
+++ src/APIService.py
@@ -0,0 +1,89 @@
+import json
+import time
+import requests
+from requests import Request, Session
+from requests.auth import HTTPBasicAuth
+import dateutil.parser
+
+
+STREAM_QUERY = "https://168.63.5.124/piwebapi/streams/{}/recorded"
+STREAM_QUERY_WITH_START_TIME = "https://168.63.5.124/piwebapi/streams/{}/recorded?startTime={}"
+STREAM_QUERY_WITH_START_END_TIME = "https://168.63.5.124/piwebapi/streams/{}/recorded?startTime={}&endTime={}"
+
+MOTOR_VALUES_WEBIDS = ["F1DPwz91T7MV00-ab1SR4aA0ZwJwAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDAxLk1PVE9SU1RST09N",
+                       "F1DPwz91T7MV00-ab1SR4aA0ZwggAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDAyLk1PVE9SU1RST09N",
+                       "F1DPwz91T7MV00-ab1SR4aA0ZwmgAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDAzLk1PVE9SU1RST09N",
+                       "F1DPwz91T7MV00-ab1SR4aA0ZwtAAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDA0Lk1PVE9SU1RST09N",
+                       "F1DPwz91T7MV00-ab1SR4aA0ZwaQAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDA1Lk1PVE9SU1RST09N"]
+
+CONTROL_LEFT_WEBIDS = ["F1DPwz91T7MV00-ab1SR4aA0ZwJAAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDAxLlNUVVJJTkcgTElOS1M",
+                       "F1DPwz91T7MV00-ab1SR4aA0ZwiAAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDAyLlNUVVJJTkcgTElOS1M",
+                       "F1DPwz91T7MV00-ab1SR4aA0ZwowAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDAzLlNUVVJJTkcgTElOS1M",
+                       "F1DPwz91T7MV00-ab1SR4aA0ZwvAAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDA0LlNUVVJJTkcgTElOS1M",
+                       "F1DPwz91T7MV00-ab1SR4aA0ZwcQAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDA1LlNUVVJJTkcgTElOS1M"]
+
+CONTROL_RIGHT_WEBIDS = ["F1DPwz91T7MV00-ab1SR4aA0ZwJQAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDAxLlNUVVJJTkcgUkVDSFRT",
+                        "F1DPwz91T7MV00-ab1SR4aA0ZwiQAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDAyLlNUVVJJTkcgUkVDSFRT",
+                        "F1DPwz91T7MV00-ab1SR4aA0ZwpAAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDAzLlNUVVJJTkcgUkVDSFRT",
+                        "F1DPwz91T7MV00-ab1SR4aA0ZwvQAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDA0LlNUVVJJTkcgUkVDSFRT",
+                        "F1DPwz91T7MV00-ab1SR4aA0ZwcgAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDA1LlNUVVJJTkcgUkVDSFRT"]
+
+PEAK_CURRENT_LEFT = [
+    "F1DPwz91T7MV00-ab1SR4aA0ZwEgAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDAxLkVORVJHSUUgT1BQRVJWTEFLVEUgTElOS1M",
+    "F1DPwz91T7MV00-ab1SR4aA0ZwfgAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDAyLklOU0NIQUtFTFBJRUtMSU5LUw",
+    "F1DPwz91T7MV00-ab1SR4aA0ZwlgAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDAzLklOU0NIQUtFTFBJRUtMSU5LUw",
+    "F1DPwz91T7MV00-ab1SR4aA0ZwsAAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDA0LklOU0NIQUtFTFBJRUtMSU5LUw",
+    "F1DPwz91T7MV00-ab1SR4aA0ZwZQAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDA1LklOU0NIQUtFTFBJRUtMSU5LUw"
+]
+
+PEAK_CURRENT_RIGHT = [
+    "F1DPwz91T7MV00-ab1SR4aA0ZwFQAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDAxLklOU0NIQUtFTFBJRUtSRUNIVFM",
+    "F1DPwz91T7MV00-ab1SR4aA0ZwfwAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDAyLklOU0NIQUtFTFBJRUtSRUNIVFM",
+    "F1DPwz91T7MV00-ab1SR4aA0ZwlwAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDAzLklOU0NIQUtFTFBJRUtSRUNIVFM",
+    "F1DPwz91T7MV00-ab1SR4aA0ZwsQAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDA0LklOU0NIQUtFTFBJRUtSRUNIVFM",
+    "F1DPwz91T7MV00-ab1SR4aA0ZwZgAAAAT1NJSEFDS0FUSE9OXFdJU1NFTDA1LklOU0NIQUtFTFBJRUtSRUNIVFM"
+]
+
+def reset_data_struct():
+    data = {
+        "motor": {"webids": MOTOR_VALUES_WEBIDS, "latest_data": []},
+        "control_left": {"webids": CONTROL_LEFT_WEBIDS, "latest_data": []},
+        "control_right": {"webids": CONTROL_RIGHT_WEBIDS, "latest_data": []},
+        "peak_current_left": {"webids": PEAK_CURRENT_LEFT, "latest_data": []},
+        "peak_current_right": {"webids": PEAK_CURRENT_RIGHT, "latest_data": []},
+    }
+
+data = {}
+
+reset_data_struct()
+
+MINIBATCH_SIZE = 50
+
+
+
+
+
+def thread_pull_data():
+
+    #dateutil.parser.parse('2019-05-11T16:15:48.1620025Z')
+    pretty_json = json.loads(resp.text)
+
+    print(json.dumps(pretty_json, indent=2))
+
+
+    while (True):
+        while (all([len(data[x]["latest_data"] for x in data.keys()) < MINIBATCH_SIZE])):
+            for x in data.keys():
+                for i in range(len(data[x]["webids"])):
+                    resp = requests.get(STREAM_QUERY_WITH_START_TIME.format(data[x]["webids"], "-1m"))
+                    if (len(resp.text) > 5):
+                        jsondata = json.loads(resp.text)
+                        items = jsondata["Items"]
+                        for j in range(len(items)):
+                            data[x]["latest_data"].append((float(items[j]["Value"]),dateutil.parser.parse(items[j]["Timestamp"])))
+
+            time.sleep(61)
+
+        #We have enough data, call Martins function and reset
+
+        reset_data_struct()
